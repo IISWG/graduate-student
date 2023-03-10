056aa75dfc7e33c3cc2fbef72481962400f59f97
muxin <2394114191@qq.com>
2023-03-10 10:12:29 +0800

    init

---
+++ src/test/java/com/example/graduatestudent/GraduateStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduatestudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
